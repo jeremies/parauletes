5c71c6c30ded169f95cbc7e0a4be84d6ef5e5316
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2021-09-12 14:13:25 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ function App() {
 
     setVerset(parauletes[numeroAleatori].versetCA);
     setCita(parauletes[numeroAleatori].cita);
+
+    fetch("https://api.countapi.xyz/hit/parauletes.netlify.app/requests");
   }, []);
 
   return (
@@ -30,7 +32,7 @@ function App() {
               <g>
                 <path
                   fill="white"
-                  class="st0"
+                  className="st0"
                   d="M17.16,0h82.72c1.82,0,3.31,1.49,3.31,3.31v92.32c-0.15,2.58-3.48,2.64-7.07,2.48H15.94 c-4.98,0-9.05,4.07-9.05,9.05c0,4.98,4.07,9.05,9.05,9.05l80.17,0v-9.63l7.07,0v12.24c0,2.23-1.82,4.05-4.05,4.05l-82.85,0 C7.33,122.88,0,115.55,0,106.59V17.16C0,7.72,7.72,0,17.16,0L17.16,0z M75.38,33.69v12.02H58.79v37.03H46.77V45.71H30.18V33.69 h16.59V19.02h12.02v14.68H75.38L75.38,33.69z"
                 />
               </g>
