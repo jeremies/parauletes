6f8c0217301cb768cc4672e0f7c19abfc3d9bdf9
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2021-09-19 21:19:23 +0200

    abreviacions llibres + tests json

---
+++ README.md
@@ -0,0 +1,3 @@
+Notes:
+
+- L'arxiu abreviacions-llibres-biblia.json té les abreviacions que usa la bíblia BCI (Biblia Catalana traducció Interconfessional)
---
+++ src/jsonTests.js
@@ -0,0 +1,37 @@
+var parauletes = require("./parauletes.json");
+var abreviacions = require("./abreviacions-llibres-biblia.json");
+
+var testos = [
+  function citesParauletesOk() {
+    let cites = parauletes.map((parauleta) => parauleta.cita);
+    let noCorrectes = cites.filter(
+      (cita) => !/^[1-9]?[A-Za-z]+\.[1-9][0-9]?[0-9]?\.[0-9-]+$/.test(cita)
+    );
+    return noCorrectes.length === 0;
+  },
+  function abreviacionsLlibresBibliaOk() {
+    let llibresParauletes = parauletes.map(
+      (parauleta) => parauleta.cita.match(/^[1-9]?[A-Za-z]+/)[0]
+    );
+    let llistaAbreviacions = abreviacions.map(
+      (abreviacio) => abreviacio.abreviacio
+    );
+    let llibresDesconeguts = llibresParauletes.filter(
+      (llibre) => !llistaAbreviacions.includes(llibre)
+    );
+    return llibresDesconeguts.length === 0;
+  },
+];
+
+var failed = 0;
+testos.forEach((func) => {
+  var resultOk = func();
+  if (!resultOk) {
+    console.log(`${func.name} no OK`);
+    failed++;
+  } else {
+    console.log(`${func.name} OK`);
+  }
+});
+
+console.log(`Number of tests failed = ${failed}`);
