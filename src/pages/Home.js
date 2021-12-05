5499b7479f955a1f410a721184ba412dd0f6d7d5
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2021-12-05 11:42:31 +0100

    test notifications

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,6 +40,24 @@ function Home() {
     ) {
       fetch("https://api.countapi.xyz/hit/parauletes.netlify.app/requests");
     }
+
+    function showNotification() {
+      debugger;
+      Notification.requestPermission(function (result) {
+        if (result === "granted") {
+          navigator.serviceWorker.ready.then(function (registration) {
+            registration.showNotification("Vibration Sample", {
+              body: "Buzz! Buzz!",
+              icon: "../images/touch/chrome-touch-icon-192x192.png",
+              vibrate: [200, 100, 200, 100, 200, 100, 200],
+              tag: "vibration-sample",
+            });
+          });
+        }
+      });
+    }
+
+    showNotification();
   }, []);
 
   const onCopyToClipboard = () => {
