31c2a6810fb84d39e61a03d2f9bf4baf5f00ecb2
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2021-12-13 10:14:36 +0100

    import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
