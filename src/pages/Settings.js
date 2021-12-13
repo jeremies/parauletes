c9a9b97b41238ab5e9118f9f56d79759e5838cf3
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2021-12-13 08:58:24 +0100

    add firebase dep and share app button

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -6,13 +6,23 @@ import ShareIcon from "@mui/icons-material/Share";
 import "./Settings.css";
 
 function Settings() {
+  const onShareApp = () => {
+    navigator.share({
+      text: "https://parauletes.netlify.app",
+    });
+  };
+
   return (
-    <div className="settings">
+    <div>
       <AppBarSettings></AppBarSettings>
       <main className="settings-body">
         <h2>Who are we?</h2>
         <p>That feels like an existential question, don't you think?</p>
-        <Button variant="outlined" startIcon={<ShareIcon />}>
+        <Button
+          variant="outlined"
+          startIcon={<ShareIcon />}
+          onClick={onShareApp}
+        >
           Compartir App
         </Button>
       </main>
