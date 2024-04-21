1c2513d4115bbeb966a68709179514174c4607aa
Joaquim Monserrat Companys <joaquim.monserrat@i3television.es>
2024-04-21 18:58:35 +0200

    feat: add PWA functionality to vite app

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/",
   build: {
-    outDir: 'build',
+    outDir: "build",
   },
-  plugins: [react()]
-})
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+});
