33f003f86bb36fb96a8981067df5af6b208cd559
Joaquim Monserrat <jmonserrat@protonmail.com>
2024-04-25 13:41:33 +0200

    feat: count PWA and android separately

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,9 +1,18 @@
+import { Device } from "@capacitor/device";
+
+const info = await Device.getInfo();
+
 export function hitCounter(key) {
-  if (
-    key === "requests"
-  ) {
+  if (key === "requests") {
+    let apikey = "";
+    if (info.platform === "web") {
+      apikey = "Jo79x+Q5SRIajvMGuXUKbA==WX1kUV4RMuFOTeNM";
+    } else {
+      apikey = "dKaL7qrDFbaPDJuTNG/0zw==RiK5bKNmTKbvCJxN";
+    }
+
     fetch(`https://api.api-ninjas.com/v1/counter?id=${key}`, {
-      headers: { "X-Api-Key": "dKaL7qrDFbaPDJuTNG/0zw==RiK5bKNmTKbvCJxN" },
+      headers: { "X-Api-Key": apikey },
     });
   }
 }
